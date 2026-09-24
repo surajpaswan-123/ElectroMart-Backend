7c196a6c7f5b7231c1a6dbbbddcfbdd4c31a866f
suraj paswan <sp437107@gmail.com>
2026-09-24 16:50:53 +0530

    feat: attach images and variants to products

--- src/main/java/electromart/ElectroMart/entity/Product.java
+++ src/main/java/electromart/ElectroMart/entity/Product.java
@@ -1,37 +1,35 @@
 package electromart.ElectroMart.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Table(name = "products")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-@JoinColumn(name = "category_id")
-private Category category;
+
+    @ManyToOne @JoinColumn(name = "category_id")
+    private Category category;
 
     private String title;
-
     private String description;
-
     private Double price;
-
     private Double oldPrice;
-
     private Integer stock;
-
     private String brand;
-
     private String imageUrl;
-
     private Double rating;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("displayOrder ASC, id ASC")
+    private List<ProductImage> images = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductVariant> variants = new ArrayList<>();
 }
